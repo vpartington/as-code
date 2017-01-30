7f4363ebc34bc018f0fe54fadacce9824ac59b35
Vincent Partington <vpartington@xebialabs.com>
2017-01-30 16:52:19 +0100

    Saving ID and title of created release in release variables.

--- Xfile-simple.groovy
+++ Xfile-simple.groovy
@@ -1,14 +1,20 @@
-xlr {
-  release("Petstore Release pipeline") {
+def parentReleaseTitle = release.title
+
+def subRelease = xlr {
+  release("Release started from " + parentReleaseTitle) {
     description "Petstore Release pipeline for various environments"
     phases {
-        phase("test") {
-          tasks {
+      phase("test") {
+        tasks {
           manual("Prepare environment") {
-              description "Prepare environment for deployment"
-            }
-}
-       }
+            description "Prepare environment for deployment"
+            team "Release Admins"
+          }
+        }
+      }
+    }
   }
 }
-}
+
+releaseVariables['SUB_RELEASE_ID'] = subRelease.id
+releaseVariables['SUB_RELEASE_TITLE'] = subRelease.title
