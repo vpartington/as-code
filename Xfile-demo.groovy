595126a4b1c0c01348093dece6c2118c6cd9e3ae
Vincent Partington <vpartington@xebialabs.com>
2017-01-31 11:41:31 +0100

    Added initial demo file.

---
+++ Xfile-demo.groovy
@@ -0,0 +1,28 @@
+import com.xebialabs.deployit.plugin.api.reflect.Type
+
+def xlDeployServer = Type.valueOf('xldeploy.XLDeployServer').descriptor.newInstance('/Configuration/Configuration1828100')
+
+def parentReleaseTitle = release.title
+
+def subRelease = xlr {
+  release("Pipeline for $parentReleaseTitle") {
+    phases {
+      phase("Deploy to prod") {
+        tasks {
+          custom('Deploy package to Prod') {
+            script {
+              type 'xldeploy.Deploy'
+              server xlDeployServer
+              deploymentPackage 'Java EE/PetClinic-ear/4.10'
+              deploymentEnvironment 'Demo/Java - WLS (Server-3)'
+            }
+            team "Release Admins"
+          }
+        }
+      }
+    }
+  }
+}
+
+releaseVariables['SUB_RELEASE_ID'] = subRelease.id
+releaseVariables['SUB_RELEASE_TITLE'] = subRelease.title
